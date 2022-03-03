34b2166174106c9fee2ee70d181e2c4c8ad84cb1
Marcus-Bengtsson <92784457+Marcus-Bengtsson@users.noreply.github.com>
2022-03-03 14:18:03 +0100

    Initial commit

---
+++ Console App/app/Program.cs
@@ -0,0 +1,23 @@
+﻿int range = 250;
+int[]? counts = new int[range];
+string text = "something";
+double totalCount = 0;
+double percentPerCount;
+while (!string.IsNullOrWhiteSpace(text))
+{
+    text = Console.ReadLine().ToLower();
+    foreach (var character in text ?? string.Empty)
+    {
+        counts[(int)character]++;
+        totalCount++;
+    }
+    percentPerCount = 100 / totalCount;
+    for (int i = 0; i < range; i++)
+    {
+        if (counts[i] > 0)
+        {
+            var character = (char)i;
+            Console.WriteLine(String.Format("{0, -3} {1, -1} {2, 3}", character, "-", Math.Round(counts[i] * percentPerCount, 2) + "%"));
+        }
+    }
+}
